e48b441a392665f765f1e35bfc6c1dd40c71fde9
Rainer Simon <aboutgeo@gmail.com>
2021-12-31 13:20:59 +0100

    Cleanup

--- src/AnnotoriousPlugin.jsx
+++ src/AnnotoriousPlugin.jsx
@@ -1,21 +1,12 @@
-import { Component } from 'react';
-import * as Annotorious from '@recogito/annotorious-openseadragon';
+import { useEffect } from 'react';
+// import * as Annotorious from '@recogito/annotorious-openseadragon';
 
-export default class AnnotoriousPlugin extends Component {
+export default props => {
 
-  constructor(props) {
-    super(props);
-  }
+  useEffect(() => {
+    console.log('effect', props.viewer)
+  }, [ props.viewer ]);
 
-  componentWillReceiveProps(next) {
-    // Used to include props.viewer!?
-    console.log('next props', next);
-    if (next.viewer)
-      Annotorious(next.viewer, {});
-  }
-
-  render() {
-    return this.props.children;
-  }
+  return props.children;
 
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import Mirador from 'mirador/dist/es/src/init'
 import AnnotoriousPlugin from './AnnotoriousPlugin';
 
-const pluginConfig = {
-  component: AnnotoriousPlugin,
-  target: 'OpenSeadragonViewer',
-  mode: 'wrap'
+const myPlugin = {
+    component: AnnotoriousPlugin,
+    target: 'OpenSeadragonViewer',
+    mode: 'wrap'
 };
 
-const miradorConfig = {
-  id: 'mirador',
-  windows: [{
-    manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
-    canvasId: 'https://iiif.harvardartmuseums.org/manifests/object/299843/canvas/canvas-47174892',
-    thumbnailNavigationPosition: 'far-bottom',
-    allowClose: false,
-  }],
-  window: {
-    allowWindowSideBar: false,
-    sideBarPanel: '',
-    sideBarOpen: true,
-  },
-  workspace: {
-    type: 'not-mosaic-or-elastic',
-  },
+const miradorCfg = {
+    id: 'mirador',
+    windows: [{
+        manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
+        canvasId: 'https://iiif.harvardartmuseums.org/manifests/object/299843/canvas/canvas-47174892',
+        thumbnailNavigationPosition: 'far-bottom',
+        allowClose: false,
+    }],
+    window: {
+        allowWindowSideBar: false,
+        sideBarPanel: '',
+        sideBarOpen: true,
+    },
+    workspace: {
+        type: 'not-mosaic-or-elastic',
+    },
 };
 
-Mirador.viewer(miradorConfig, [ pluginConfig ]);
-
+Mirador.viewer(miradorCfg, [myPlugin]);
