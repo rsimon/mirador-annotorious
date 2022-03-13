72132c045eb888bcf77d8a54b137cbb6ae2497be
Rainer Simon <aboutgeo@gmail.com>
2022-03-13 14:48:12 +0100

    Paginated annotations

--- src/AnnotoriousPlugin.jsx
+++ src/AnnotoriousPlugin.jsx
@@ -1,13 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as Annotorious from '@recogito/annotorious-openseadragon';
 
 export default props => {
 
+  const [ annotations, setAnnotations ] = useState({});
+
   useEffect(() => {
     if (props.viewer) {
       const anno = Annotorious(props.viewer);
+
+      const existing = annotations[props.label] || [];
+      anno.setAnnotations(existing);
+
+      anno.on('createAnnotation', annotation => {
+        setAnnotations({
+          ...annotations,
+          [props.label]: [...existing, annotation ]
+        });
+      });
+
+      anno.on('updateAnnotation', annotation => {
+        setAnnotations({
+          ...annotations,
+          [props.label]: existing.map(a => a.id === annotation.id ? 
+            annotation : a)
+        });
+      });
+      
+      anno.on('deleteAnnotation', annotation => {
+        setAnnotations({
+          ...annotations,
+          [props.label]: existing.filter(a => a.id !== annotation.id)
+        });
+      });
+
+      return (() => anno.destroy());
     }
-  }, [ props.viewer ]);
+  }, [ props.viewer, props.label ]);
 
   return props.children;
 
--- src/index.js
+++ src/index.js
@@ -4,27 +4,31 @@ import AnnotoriousPlugin from './AnnotoriousPlugin';
 import '@recogito/annotorious-openseadragon/dist/annotorious.min.css';
 
 const myPlugin = {
-    component: AnnotoriousPlugin,
-    target: 'OpenSeadragonViewer',
-    mode: 'add'
+  component: AnnotoriousPlugin,
+  target: 'OpenSeadragonViewer',
+  mode: 'add',
+  mapStateToProps: (state, props) => { 
+    // TODO extract annotations?
+    return {};
+  }
 };
 
 const miradorCfg = {
-    id: 'mirador',
-    windows: [{
-        manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
-        canvasId: 'https://iiif.harvardartmuseums.org/manifests/object/299843/canvas/canvas-47174892',
-        thumbnailNavigationPosition: 'far-bottom',
-        allowClose: false,
-    }],
-    window: {
-        allowWindowSideBar: false,
-        sideBarPanel: '',
-        sideBarOpen: true,
-    },
-    workspace: {
-        type: 'not-mosaic-or-elastic',
-    },
+  id: 'mirador',
+  windows: [{
+    manifestId: 'https://iiif.harvardartmuseums.org/manifests/object/299843',
+    canvasId: 'https://iiif.harvardartmuseums.org/manifests/object/299843/canvas/canvas-47174892',
+    thumbnailNavigationPosition: 'far-bottom',
+    allowClose: false,
+  }],
+  window: {
+    allowWindowSideBar: false,
+    sideBarPanel: '',
+    sideBarOpen: true,
+  },
+  workspace: {
+    type: 'not-mosaic-or-elastic',
+  }
 };
 
 Mirador.viewer(miradorCfg, [myPlugin]);
